23ce29ef58a7a921249a3b98253ba71463956faf
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2021-10-02 19:01:09 -0500

    Displaying course info in the grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
 import React from 'react';
+import './App.css';
 
 const schedule = {
-  title: "CS Courses for 2018-2019"
+  'title': "CS Courses for 2018-2019",
+  "courses": {
+    "F101" : {
+      "id" : "F101",
+      "meets" : "MWF 11:00-11:50",
+      "title" : "Computer Science: Concepts, Philosophy, and Connections"
+    },
+    "F110" : {
+      "id" : "F110",
+      "meets" : "MWF 10:00-10:50",
+      "title" : "Intro Programming for non-majors"
+    },
+    "S313" : {
+      "id" : "S313",
+      "meets" : "TuTh 15:30-16:50",
+      "title" : "Tangible Interaction Design and Learning"
+    },
+    "S314" : {
+      "id" : "S314",
+      "meets" : "TuTh 9:30-10:50",
+      "title" : "Tech & Human Interaction"
+    }
+  }
 };
 
+const Banner = ({ title }) => (
+  <h1>{ title }</h1>
+);
+
+const CourseList = ({ courses }) => (
+  <div className="course-list">
+  { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+  </div>
+);
+
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+);
+
+const Course = ({ course }) => (
+  <div className="card m-2 p-1">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
+  </div>
+);
+
 const App = () =>  (
-  <div>
-    <h1>{ schedule.title }</h1>
+  <div className="container">
+    <Banner title={ schedule.title } />
+    <CourseList courses={ schedule.courses } />
   </div>
 );
 
